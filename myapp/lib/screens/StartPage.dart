3b6bfc92c25c47c2795150c4d0ab1f461b2b5285
Gaurav Jha <jhagaurav667@gmail.com>
2021-07-13 11:35:24 +0530

    created animated container for start page

--- myapp/lib/screens/StartPage.dart
+++ myapp/lib/screens/StartPage.dart
@@ -5,26 +5,59 @@ void main() {
   runApp(start());
 }
 
-class start extends StatelessWidget {
+class start extends StatefulWidget {
   const start({Key? key}) : super(key: key);
 
   @override
+  _startState createState() => _startState();
+}
+
+class _startState extends State<start> {
+  bool buttonChange = false;
+  @override
   Widget build(BuildContext context) {
     return Material(
-      child: SingleChildScrollView(
-          child: Column(children: [
+        child: SingleChildScrollView(
+            child: Column(
+      children: [
         Image.asset("assets/images/welcome.png"),
         Divider(color: Colors.green, thickness: 5),
-        SizedBox(height: 150),
-        ElevatedButton(
-            child: Text("Click here ......",
-                style: TextStyle(color: Colors.white)),
-            onPressed: () {
+        SizedBox(height: 350),
+        InkWell(
+            child: AnimatedContainer(
+              duration: Duration(seconds: 1),
+              height: 90,
+              width: buttonChange ? 90 : 250,
+              child: Center(
+                child: Text("Lets Go....",
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    )),
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50.0),
+                color: Colors.green,
+              ),
+            ),
+            onTap: () {
+              setState(() {
+                buttonChange = true;
+              });
+
               Navigator.push(
                   context, MaterialPageRoute(builder: (context) => login()));
-            },
-            style: ElevatedButton.styleFrom(minimumSize: Size(200.0, 60.0)))
-      ])),
-    );
+            })
+      ],
+    ))
+        // ElevatedButton(
+        //     child: Text("Click here ......",
+        //         style: TextStyle(color: Colors.white,fontSize: 20)),
+        //     onPressed: () {
+        //       Navigator.push(
+        //           context, MaterialPageRoute(builder: (context) => login()));
+        //     },
+        //     style: ElevatedButton.styleFrom(minimumSize: Size(200.0, 60.0)),)
+        );
   }
 }
