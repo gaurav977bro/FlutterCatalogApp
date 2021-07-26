5fea45806732f0735abac5366b1b97fda8ba7447
Gaurav Jha <jhagaurav667@gmail.com>
2021-07-26 17:55:19 +0530

    correction

--- myapp/lib/screens/HomePage.dart
+++ myapp/lib/screens/HomePage.dart
@@ -20,15 +20,7 @@ class Home extends StatelessWidget {
           "Catalog App",
         )),
         // HOME PAGE BODY
-        body: Container(
-            child: Center(
-                child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(minimumSize: Size(150, 50)),
-                    child: Text("Exit"),
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => login()));
-                    }))),
+        body:Container(),
 
         // DRAWER
         drawer: MyDrawer());
