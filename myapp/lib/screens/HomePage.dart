8f9735bf6b67ba19e9afc7f5ea4f11b35ab5e3a1
Gaurav Jha <jhagaurav667@gmail.com>
2021-08-03 14:17:48 +0530

    grid view and grid tile complete

--- myapp/lib/screens/HomePage.dart
+++ myapp/lib/screens/HomePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:myapp/models/catalog.dart';
 import "dart:convert";
@@ -45,22 +46,43 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
         // APP BAR
-        appBar: AppBar(
-            title: Text(
-          "Catalog App",
-        )),
+        appBar: AppBar(title: Text("Catalog App")),
         // HOME PAGE BODY
-        body: Padding(
-          padding:EdgeInsets.symmetric(horizontal: 5, vertical:5 ),
-          child: CatalogModel.items.isNotEmpty? ListView.builder(
-              itemCount: CatalogModel.items.length,
-              itemBuilder: (context, index) {
-                return ItemWidget(items: CatalogModel.items[index]);
-              }
-              ): Center(child: CircularProgressIndicator()),
-        )
-            ,
-
+        body: CatalogModel.items.isNotEmpty
+            ? GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 2,
+                ),
+                itemCount: CatalogModel.items.length,
+                itemBuilder: (context, index) {
+                  final values = CatalogModel.items[index];
+                  return Card(
+                      shadowColor: Colors.red,
+                      elevation: 10,
+                      clipBehavior: Clip.hardEdge,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5.0)),
+                      color: Colors.pink,
+                      child: GridTile(
+                          header: Container(padding:EdgeInsets.all(10) ,height:34 ,child:Text(values.name,style:TextStyle(color:Colors.white)),decoration:BoxDecoration(color:Colors.pink)),
+                          footer: Container(
+                            color:Colors.pink,
+                            padding:EdgeInsets.all(10),
+                            child: Text(
+                              "\$${values.price}",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          child: Image.network(values.image,
+                              filterQuality: FilterQuality.high,
+                              fit: BoxFit.fill)));
+                })
+            : Center(child: CircularProgressIndicator(color: Colors.pink)),
         // DRAWER
         drawer: MyDrawer());
   }
--- myapp/lib/widgets/BarTheme.dart
+++ myapp/lib/widgets/BarTheme.dart
@@ -3,11 +3,15 @@ import "package:flutter/material.dart";
 class MyTheme {
   static ThemeData lightTheme(BuildContext context) => ThemeData(
       primarySwatch: Colors.pink,
-      brightness: Brightness.dark,
+      // brightness: Brightness.dark,
       appBarTheme: AppBarTheme(
-        iconTheme: IconThemeData(color: Colors.white),
         
-        elevation: 5.0,
+        
+        
+        
+        iconTheme: IconThemeData(color: Colors.white,),
+        
+        
         
       ),
       );
